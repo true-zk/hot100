55f9dc8ba265a5985d4c7bd794d49e1884eea2d3
KenZhong <z1519187715@163.com>
2025-10-01 22:02:11 +0800

    10.1

---
+++ dailycollection/几何类/9-27.largestTriangleArea.cpp
@@ -0,0 +1,23 @@
+/*
+    812. 最大三角形面积
+    easy？？？
+
+    给你一个由 X-Y 平面上的点组成的数组 points ，其中 points[i] = [xi, yi] 。
+    从其中取任意三个不同的点组成三角形，返回能组成的最大三角形的面积。
+    与真实值误差在 10-5 内的答案将会视为正确答案。
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+    本题本质上为凸包最大问题
+
+*/
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+
+    }
+};
--- 代码随想录/1array/2.binaryseach.cpp
+++ 代码随想录/1array/2.binaryseach.cpp
@@ -1,20 +1,95 @@
+/*
+    二分有两种写法：[l, r] 和 [l, r)
+*/
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+class BinarySearch {
+public:
+    /*
+        [l, r] 写法
+    */
+    int binarySearch1(vector<int> &arr, int l, int r, int tgt) {
+        while (l <= r) {    // 闭区间，l == r有意义
+            int mid = l + (r - l) / 2;
+            if (arr[mid] > tgt) {
+                r = mid - 1;    // 闭区间，mid一定不是下一个闭区间
+            }
+            else if (arr[mid] < tgt) {
+                l = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 
+    /*
+        [l, r) 写法
+    */
+   int binarySearch2(vector<int> &arr, int l, int r, int tgt) {
+        while (l < r)   // 开区间，l == r的时候没有元素
+        {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] > tgt) {
+                r = mid;    // 右开区间
+            }
+            else if (arr[mid] < tgt) {
+                l = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+   }
+};
+
+///////////////////////////////////////////////
 class Solution {
 public:
+    /*
+        [l, r]
+        下界就是 第一个 >=tgt 的idx
+        ... tgt [lower_bound ...
+
+        闭区间，所以大于和小于的情况都好写，
+        关键是==的时候，r = mid - 1;
+    */
     int lowerbound(vector<int> &arr, int l, int r, int tgt) {
         while (l <= r)
         {
             int mid = l + (r - l) / 2;
             if (arr[mid] >= tgt) {
-                r = mid - 1;
+                r = mid - 1;    // 相等，r会到mid = 1，后续l = mid + 1会自然移到lower_bound
             }
             else {
                 l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    /*
+        [l, r]
+        上界就是 第一个 >tgt 的idx
+        ... tgt (upper_bound ...
+
+        闭区间，所以大于和小于的情况都好写，
+        关键是==的时候，l = mid + 1;
+    */
+    int upperbound(vector<int> &arr, int l, int r, int tgt) {
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] > tgt) {
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;    // 相等，l = mid + 1，就是上界
             }
         }
         return l;
